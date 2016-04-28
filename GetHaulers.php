60c6664622984a070eaf6330e919493e5276ce40
lgarc025 <lgarc025@ucr.edu>
2016-04-28 21:21:23 +0000

    First Commit

---
+++ GetHaulers.php
@@ -0,0 +1,54 @@
+<?php
+
+	function get_broker()
+	{
+			$str='';
+			$str=$_POST['std'];
+			$names = get_data($std);
+
+			foreach ($names as $name)
+			{
+					$str.='<option value="'.$name.'">'.$name.'</option>';
+			}
+
+			return $str;
+	}
+
+	function get_data ($std)
+	{
+			 
+			 //Create a Broker Array
+			 $Brokers=array();
+
+			 $servername = "luis.cylcbbatmizc.us-west-2.rds.amazonaws.com";
+			 $username = "Luis";
+			 $password = "Luis1234";
+			 $dbname = "LuisTrucking";
+			
+			 $conn = new mysqli($servername, $username, $password, $dbname);
+			 $sql = "SELECT HaulerName FROM Hauler";
+			 $result = $conn->query($sql);
+
+			 while($row = $result->fetch_assoc())
+			 {
+					 //Load an Array wih the brokers
+					 $Brokers[] = $row[HaulerName];
+			 }
+
+			 return $Brokers;
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<select name="Brokers">
+<?php echo get_broker(); 
+?>
+</select>
+</body>
+</html>
+
