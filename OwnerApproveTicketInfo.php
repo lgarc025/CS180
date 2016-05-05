9ebdaa958adc0d59e764a6293ed8a5eb267162f1
root <root@ip-172-31-43-222.us-west-2.compute.internal>
2016-05-05 03:44:43 +0000

    Aprove Lists

---
+++ OwnerApproveTicketInfo.php
@@ -0,0 +1,49 @@
+<?php
+$servername = "luis.cylcbbatmizc.us-west-2.rds.amazonaws.com";
+$username = "Luis";
+$password = "Luis1234";
+$dbname = "LuisTrucking";
+
+$user =$_POST["driver"];
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//Run Query to authenticate User
+if($user == '*')
+{
+	 $sql = "SELECT * FROM Ticket Where Approval='0' ORDER BY TicketDate DESC";
+}
+else
+{
+	 $sql = "SELECT * FROM Ticket Where DriverName ='$user' and Approval='0' ORDER BY TicketDate DESC";
+}
+
+$result = $conn->query($sql);
+
+
+echo "<table id='t01' class='center'>"; // start a table tag in the HTML
+echo "<tr> <th>Approve</th><th>TicketID</th> <th>Date</th> <th>TruckID</th> <th>Driver</th> <th>Hauler</th> <th>Broker</th> <th>Rate</th> <th>Tons/Hours</th> <th>Total</th>   </tr> ";
+
+while($row = $result->fetch_assoc())
+{
+		$Approval = "Approved";
+		if ($row['Approval'] == 0 )
+		{
+				$Approval="Not Approved";
+
+		}
+		echo "<tr><td>" ." <input type='checkbox' id='".$row['TicketID']."'</td><td>"  . $row['TicketID'] . "</td><td>" . $row['TicketDate']  . "</td><td>" . $row['TruckID'] . "</td><td>". $row['DriverName']. "</td><td>" . $row['HaulerName'] . "</td><td>" . $row['BrokerName'] . "</td><td>" ."$" .$row['Rate'] . "</td><td>" . $row['Amount'] . "</td><td>" ." $". $row['Total']  ."</tr></td>";
+		
+
+}
+
+$conn->close();
+echo "</table>"; //Close the table
+
+?>
---
+++ OwnerGenRepDisplayBrokerTicket.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Luis Trucking - Sign In </title>
+
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" href="css/styleDisplayDriverData.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+<style>
+
+body {margin:0;}
+
+ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #333;
+		position: fixed;
+		top: 0;
+		width: 100%;
+}
+
+li {
+		float: left;
+}
+
+li a {
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+}
+
+li a:hover:not(.active) {
+		background-color: #88;
+}
+
+.active {
+		background-color: #888;
+}
+
+</style>
+</head>
+<body>
+<ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#news">Haulers</a></li>
+	<li><a href="#contact1">Brokers</a></li>
+	<li><a class="active" href="#about3">Drivers</a></li>
+    <li><a href="#home4">Trucks</a></li>
+    <li><a href="#news3">Expenses</a></li>
+	<li><a href="luistrucking.html">Logout</a></li>
+</ul>
+
+
+     <div class="container">
+		<div class="top">
+			<h1 id="title" class="hidden"><span id="logo"><br/> </h1>
+		</div>
+		<div class="ticket-box animated fadeInUp">
+			<div class="box-header">
+				<h2>Broker Dues By Date</h2>
+			<form action="OwnerGenRepDisplayBrokerTicket.php" method="post">
+			</div>
+			<label for="broker">Select Broker:</label>
+			<br/>
+			<select name='broker' id='broker'>
+			<?php
+				require('./GetBrokers.php');
+			?>
+			</select>
+			<br/>
+            <br/>
+			<label for="time">Start Date:</label>
+			<br/>
+			<input type="date" name="startdate" id="startdate">
+			<br/>
+			<label for="time">End Date:</label>
+			<br/>
+			<input type="date" name="enddate" id="enddate">
+			<br/>
+			<button type="submit">Generate Report</button>
+			<br/>
+			<br/>
+			<?php include('OwnerDisplayBrokerTicketInfo.php'); ?>
+			<br/>
+			</form>
+		</div>
+	</div>
+
+</body>
+
+<script>
+	$(document).ready(function () {
+    	$('#logo').addClass('animated fadeInDown');
+    	$("input:text:visible:first").focus();
+	});
+	$('#username').focus(function() {
+		$('label[for="username"]').addClass('selected');
+	});
